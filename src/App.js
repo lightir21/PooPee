a537040940232bf2a6feb9f48447079d624af8c6
Meir Meir <lightir21@gmail.com>
2021-12-11 09:35:43 +0200

    modified:   src/App.js 	modified:   src/Poo.js 	src/Form.js
    	#

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Poo from "./Poo";
 import Feed from "./Feed";
 import Pee from "./Pee";
 import Navbar from "./Navbar";
+import { useState } from "react/cjs/react.development";
 
 const App = () => {
+  const [poo, setPoo] = useState("");
+  const [pee, setPee] = useState("");
+  const [feed, setFeed] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="feed" element={<Feed />} />
-        <Route path="poo" element={<Poo />} />
+        <Route path="poo" element={<Poo poo={poo} setPoo={setPoo} />} />
         <Route path="pee" element={<Pee />} />
       </Routes>
     </>
